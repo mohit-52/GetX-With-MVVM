811763ad24102181b39e04ef4e04ea86fca63c12
Mohit <mohitkaushis7@gmail.com>
2023-07-04 22:19:55 +0530

    Assets, Fonts & Icons Added

--- lib/view/splash_view.dart
+++ lib/view/splash_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:getx_mvvm/data/app_exceptions.dart';
+import 'package:getx_mvvm/res/assets/image_assets.dart';
+import 'package:getx_mvvm/res/fonts/app_fonts.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -13,7 +15,13 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('email_hint'.tr),),
+      appBar: AppBar(title: Text('email_hint'.tr, style: TextStyle(fontFamily: AppFonts.schylerRegular),),),
+      body: Column(
+        children: [
+          Image(image: AssetImage(ImageAssets.splashScreen)),
+          Image(image: AssetImage(ImageAssets.rocketIcon)),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){
           throw InternetException("Found");
